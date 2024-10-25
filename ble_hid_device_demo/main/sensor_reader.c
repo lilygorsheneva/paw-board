eff486eb4b996c2bdc33954f4d3562aed07759e3
Lily <lilygorsheneva@gmail.com>
2024-10-24 21:00:22 -0700

    Rewrote criteria for send and repeat, greatly reducing error rate. Feedback scales with number of "keys" pressed.

--- ble_hid_device_demo/main/sensor_reader.c
+++ ble_hid_device_demo/main/sensor_reader.c
@@ -25,6 +25,7 @@ static adc_channel_t channels[] = {ADC_CHANNEL_0, ADC_CHANNEL_1, ADC_CHANNEL_2, 
 static int adc_raw[10];
 static int analog_values[5];
 static bool pins_pressed[5];
+static bool pins_unstable[5];
 static int thresholds[5];
 static int debounce[5];
 
@@ -33,7 +34,7 @@ static char out = 0;
 
 //microseconds
 static int wait_to_confirm_input = 300* 1000;
-static int key_repeat_delay = 5000* 1000;
+static int send_feedback_time = 300 * 1000;
 
 #define LEDC_TIMER              LEDC_TIMER_0
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
@@ -71,6 +72,9 @@ static void example_ledc_init(void)
 #define GPIO_OUTPUT_IO_0    11
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0))
 
+
+// Naive approach to handling a read. Hardcoded threshold with 
+// a small debouce window later.
 void processInputPin(uint8_t i) {
   int value = adc_raw[i];
   analog_values[i] = value;
@@ -85,14 +89,61 @@ void processInputPin(uint8_t i) {
   }
 }
 
+
+void setInputThresholds(){
+    thresholds[0] = 20;
+    thresholds[1] = 20;
+    thresholds[2] = 20;
+    thresholds[3] = 20;
+    thresholds[4] = 20;
+
+    debounce[0] = 30;
+    debounce[1] = 30;
+    debounce[2] = 50;
+    debounce[3] = 20;
+    debounce[4] = 10;
+
+}
+
+// 
+void processInputPinStabilityMode(uint8_t i) {
+
+  // Maybe this can be dynamic
+  const int noise_floor = 10;
+
+  int value = adc_raw[i];
+  if (value <= noise_floor) {
+    pins_pressed[i] = false;
+    pins_unstable[i] = false;
+    analog_values[i] = value;
+    return;
+  }
+
+  if (!pins_pressed[i]) {
+    if (abs((float)(value - analog_values[i])/(float)(analog_values[i])) <= 0.1) {
+      pins_pressed[i] = true;
+      pins_unstable[i] = false;
+    }
+  }
+  else {
+    if (abs((float)(value - analog_values[i])/(float)(analog_values[i])) >= 0.1) {
+      pins_unstable[i] = true;
+      pins_pressed[i] = true;
+    }
+  }
+
+  analog_values[i] = value;
+}
+
 void processInputPins(void) {
   for (int i = 0; i < 5; ++i) {
-    processInputPin(i);
+    processInputPinStabilityMode(i);
   }
 }
 
 char decode(void) {
   char buf = 0;
+
   for (int i = 0; i < 5; ++i) {
     buf = buf + (pins_pressed[i] * (1 << i));
   }
@@ -102,6 +153,41 @@ char decode(void) {
   return buf;
 }
 
+int pins_pressed_count(void) {
+  char buf = 0;
+
+  for (int i = 0; i < 5; ++i) {
+    buf = buf + pins_pressed[i];
+  }
+  return buf;
+}
+
+void scale_vibration_to_pincount(bool force_off, bool force_full){
+
+  uint32_t duty = (1 << 13) -1;  
+  if (force_off) {
+    duty = (1 << 13) -1;  
+  } else if (force_full) {
+    duty = 0;
+  } else {
+    int c = pins_pressed_count();
+    const uint32_t duty_increment = (1 << 13)/12 -1;  
+    duty = (6 - c) * duty_increment;
+  }
+
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, duty));
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+
+}
+
+bool all_pins_stable(void){
+  bool unstable = false;
+  for (int i = 0; i < 5; ++i) {
+    unstable = unstable & pins_unstable[i];
+  }
+  return !unstable;
+}
 
 void writeOutputPins(bool vibrate[5]) {
   // TODO multipin feedback
@@ -121,7 +207,10 @@ int64_t gettime(){
 char decodeAndUpdate(void) {
   static char _last_char_value = 0;
   static unsigned long _accept_input_at;
-  static unsigned long _repeat_delay_threshold = 0;
+  static unsigned long _post_send_vibrate = 0;
+
+  // Attempting to prevent key repeat by requriing all to be unpressed.
+  static bool _sent = false;
 
   uint64_t last_time = _current_time;
 
@@ -129,47 +218,50 @@ char decodeAndUpdate(void) {
   
   char out = decode();
 
+
+
   // Nothing Pressed
   if (!out) {
     _last_char_value = 0;
+    _sent = false;
 
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, (1 << 13) -1));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-
+    scale_vibration_to_pincount(true, false);
     return 0;
   }
+  if (_sent) {
+    scale_vibration_to_pincount(true, _post_send_vibrate > _current_time);
+    return 0;
+  }
+
+  if (!all_pins_stable()) {
+    scale_vibration_to_pincount(false, false);
+    return 0;
+  }
+
+  // Avoiding key repeat by requring full release.
+
 
   // Something Pressed, but different from last input
   if (_last_char_value != out) {
     _accept_input_at = _current_time + wait_to_confirm_input;
-    _last_char_value = out;
-    
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, (1 << 12)));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-
+    _last_char_value = out; 
+    scale_vibration_to_pincount(false, false);
     return 0;
   }
 
   // Pressed, but not long enough to be an input.
   if (_current_time < _accept_input_at) {
-
-
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0 ));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-
-
+    scale_vibration_to_pincount(false, false);
     return 0;
   }
 
   // Send key.
-    _accept_input_at = _current_time + key_repeat_delay;
-  
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, (1 << 12)  ));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
 
 
-
-  return out;
+    _sent = true;
+    _post_send_vibrate = _current_time + send_feedback_time;
+    scale_vibration_to_pincount(false, true);
+    return out;
 }
 
 
@@ -193,19 +285,7 @@ void pressure_sensor_init(void)
       ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, channels[i], &config));
     }
       
-
-    thresholds[0] = 120;
-    thresholds[1] = 120;
-    thresholds[2] = 250;
-    thresholds[3] = 80;
-    thresholds[4] = 40;
-
-    debounce[0] = 30;
-    debounce[1] = 30;
-    debounce[2] = 50;
-    debounce[3] = 20;
-    debounce[4] = 10;
-
+    setInputThresholds();
 
     // //zero-initialize the config structure.
     // gpio_config_t io_conf = {};
@@ -226,15 +306,20 @@ void pressure_sensor_init(void)
 }
 
 char convert_to_hid_code(char in){
-if (in == 31) {
-  return  HID_KEY_DELETE;
-} else if (in > 'z' - 'a' + 1) {
-  return HID_KEY_SPACEBAR;
-}
-else {
-  return in + HID_KEY_A - 1;
-}
+  if (in == 0){return 0;}
 
+  // TODO: switch statement
+  if (in == 31) {
+    return  HID_KEY_ENTER;
+  }else if (in == 30){
+    return  HID_KEY_DELETE;
+  }
+  else if (in > 'z' - 'a' + 1) {
+    return HID_KEY_SPACEBAR;
+  }
+  else {
+    return in + HID_KEY_A - 1;
+  }
 }
 
 char pressure_sensor_read(void) {
@@ -245,7 +330,10 @@ char pressure_sensor_read(void) {
      
         processInputPins();
         char out = decodeAndUpdate();
-        ESP_LOGI(TAG, "| %4d | %4d | %4d | %4d | %4d | %c |", analog_values[0], analog_values[1], analog_values[2], analog_values[3], analog_values[4], out + 'a' - 1);
+
+        if (analog_values[0] || analog_values[1]  || analog_values[2] || analog_values[3]  || analog_values[4] ) {
+          ESP_LOGI(TAG, "| %4d | %4d | %4d | %4d | %4d | %c |", analog_values[0], analog_values[1], analog_values[2], analog_values[3], analog_values[4], out + 'a' - 1);
+        }
 
         return convert_to_hid_code(out);
     
